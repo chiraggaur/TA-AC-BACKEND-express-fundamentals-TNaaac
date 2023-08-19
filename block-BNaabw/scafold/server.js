214a41d60d2c31f39c974e9c9d73e751239a783e
chirag gaur <chiragwalker@gmail.com>
2023-08-19 21:19:35 +0530

    error

---
+++ block-BNaabw/scafold/server.js
@@ -0,0 +1,47 @@
+let express = require("express");
+let app = express();
+let logger = require("morgan");
+let cookieParser = express("cookie-parser");
+
+// middlewares
+
+//build - in
+
+app.use(express.json()); // capture json data
+app.use(express.urlencoded()); // capture forms data
+app.use(express.static(__dirname + "public")); // handling static assets
+
+// 3rd party middlewares
+
+app.use(logger("div"));
+app.use(cookieParser());
+
+// error on client/server middlewares
+
+app.use("/admin", (req, res) => {
+  next("server error unauthorised");
+});
+
+// routes
+
+app.get("/", (req, res) => {
+  res.send("Welcome to website ");
+});
+
+app.get("/users", (req, res) => {
+  res.send(" user is here! ");
+});
+
+// error middlewares
+
+app.use((req, res) => {
+  res.send("page not found"); // when rout is not handled by server
+});
+
+app.use((err, req, res, next) => {
+  res.send(err);
+});
+
+app.listen(3000, () => {
+  console.log("server is live ");
+});
